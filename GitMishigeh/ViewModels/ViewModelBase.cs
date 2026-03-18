d4bd50fef2514b0df9e519e4e2f2f05459145146
Avrumi Friedman <a.friedman07@gmail.com>
2026-03-18 14:40:27 -0400

    Initial commit

---
+++ GitMishigeh/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GitMishigeh.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ GitMishigeh/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GitMishigeh.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
